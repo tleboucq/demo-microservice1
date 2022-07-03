ada99ed0d8e9b8eb97aa62a4a4b6422b79cd430d
Samy Fadel <samyfadel@google.com>
2022-07-03 18:29:13 +0200

    samy

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 80;
-app.get('/', (req, res) => {
+app.get('/microservice1', (req, res) => {
     res.send('<h1>microservice1!</h1>');
 });
 app.listen(port, () => {
