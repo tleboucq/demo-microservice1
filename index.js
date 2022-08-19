7724b1f016bf2155b629e558990bf379a595c9c9
Samy Fadel <samyfadel@google.com>
2022-08-19 17:23:51 +0200

    samy

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/microservice1', (req, res) => {
     res.send('<h1>microservice1!</h1>');
 });
 
+app.get('/v2', (req, res) => {
+    console.log(req.url);
+    res.send('<h1>microservicev22!</h1>');
+});
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
